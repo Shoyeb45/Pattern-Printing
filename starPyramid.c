d2ce75c971fe2a432656567a6b810a8365242761
Shoyeb45 <shoyebff45@gmail.com>
2023-12-12 23:57:32 +0530

    new

--- starPyramid.c
+++ starPyramid.c
@@ -3,20 +3,17 @@
 void main()
 {
     int n=4;
-
-    for(int i=1; i<=n ; i++ ){
-        for(int j=1; j<=n-i ; j++){
-            printf(" "); //For printing space          
-        }
-         for(int k=1 ; k<=n-i ; k++){
-                printf("*"); //Printing stars  
-            }
-                     for(int u=1 ; u<=i ; u++){
-                printf("*"); //Printing stars  
-            }
-            for(int l=1; l<=n-i ; l++){
-                printf(" ");
-            }
-        printf("\n");
+int cnt=1;
+  for(int i=1; i<=n ; i++){
+    for(int j=1 ; j<=n-i ; j++){
+        printf(" ");
     }
-}
+    
+    for(int k=1 ; k<=cnt ; k++){
+        printf("*");
+    }
+    cnt+=2;
+    printf("\n");
+  }
+    
+    }
